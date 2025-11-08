324e5f67e662a085033c5ceef6cc2505726500f9
cryptogit123 <cryptocal999@gmail.com>
2025-11-08 15:02:06 +0530

    testing can we push directly to develop

--- index.ts
+++ index.ts
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello Worldvvvv");
 });
 
+
+const promisedSetTimeout = (delay: number) => {
+  return new Promise((res, rej) => {
+    setTimeout(res,delay)
+  })
+}
+
+promisedSetTimeout(1000).then(() => console.log('promise resolved'))
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
